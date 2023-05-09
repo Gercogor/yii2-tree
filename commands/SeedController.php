61eb1739d67d607069348b4bc785cd459ea00fff
Gercogor <gercogor@gmail.com>
2023-05-09 15:17:55 +0700

    db seeder

---
+++ commands/SeedController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\commands;
+
+use app\models\Tree;
+use Faker\Factory;
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+class SeedController extends Controller
+{
+    const COUNT_OF_RECORDS = 100;
+
+    /**
+     * @return int Exit code
+     */
+    public function actionIndex()
+    {
+        echo 'start seed' . "\n";
+
+        $faker = Factory::create();
+
+        $tree = new Tree();
+
+        $currentTree = Tree::find()->asArray()->all();
+
+        if (count($currentTree) >= self::COUNT_OF_RECORDS) {
+            echo 'db ready' . "\n";
+            return ExitCode::OK;
+        }
+
+        for ($i = 0; $i < self::COUNT_OF_RECORDS; $i++) {
+            $tree->setIsNewRecord(true);
+            $tree->id = null;
+            $tree->name = $faker->name();
+            $digit = $faker->optional()->passthrough(mt_rand(0, $i));
+            $tree->parent_id = $digit < $i ? $digit : 0;
+            $tree->save();
+        }
+
+        echo 'seed complete, db ready' . "\n";
+
+        return ExitCode::OK;
+    }
+}
